7ead0d5ca9aa89ad92266bb2d2752cc37ab1c37e
Cory Lewis <cjl9703@yahoo.com>
2017-11-20 15:37:23 -0800

    Have I created a monster?

--- testExtension/content.js
+++ testExtension/content.js
@@ -1,4 +1,9 @@
-var images = document.getElementsByTagName('img');
-for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
-}
+    var images = document.getElementsByTagName('img');
+    for (let i = 0, l = images.length; i < l; i++) {
+      images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+    }
+
+    var element = document.getElementsByTagName('p');//grab all element tags
+    for(let i = 0, l = element.length; i < l; i++){
+      element[i].innerHTML = "I'm Pickle Rick!!!!!!!!!!!!!!!";
+    }
