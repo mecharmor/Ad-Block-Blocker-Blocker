fb7a1d75b29bbf840c84724adac35a886ebd25d9
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2018-01-28 18:39:25 -0800

    Added a MutationObserver and fixed the loading in problem (hopefully)

--- testExtension/content.js
+++ testExtension/content.js
@@ -1,9 +1,48 @@
-    var images = document.getElementsByTagName('img');
-    for (let i = 0, l = images.length; i < l; i++) {
-      images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
-    }
 
-    var element = document.getElementsByTagName('p');//grab all element tags
-    for(let i = 0, l = element.length; i < l; i++){
-      element[i].innerHTML = "I'm Pickle Rick!!!!!!!!!!!!!!!";
-    }
+// Variables
+let	ob;
+
+window.addEventListener("load", function()	{
+	ob=	new MutationObserver(function(args)	{
+		for(let a= 0; a< args.length; a++)	{
+			for(let b= 0; b< args[a].addedNodes.length; b++)	{
+				if(!args[a].addedNodes[b].getElementsByTagName)
+					continue;
+				
+				// Variables
+				let	imgs=	args[a].addedNodes[b].getElementsByTagName("img");
+				
+				for(let c= 0; c< imgs.length; c++)	{
+					try{
+					  imgs[c].src = 'https://placekitten.com/' + imgs[c].width + '/' + imgs[c].height;
+					  // Placeholder one has a better image system than placekitten because there are not enough kitten photos
+					  /*if(imgs[c].width== imgs[c].height== 0)
+						imgs[c].src="https://via.placeholder.com/100x100";
+					  else
+						imgs[c].src = 'https://via.placeholder.com/' + imgs[c].width + 'x' + imgs[c].height;*/
+					}catch(e){console.log(e);}
+				}
+			}
+		}
+	});
+	ob.observe(document.body, {
+		childList:	true,
+		subtree:	true,
+		attributes:	true
+	});
+	
+	setTimeout(changeImages, 5000);
+});
+
+function changeImages()	{
+	// Variables
+	let	imgs=	document.getElementsByTagName("img");
+
+	for(let b= 0; b< imgs.length; b++)	{
+		try{
+		  imgs[b].src = 'https://placekitten.com/' + imgs[b].width + '/' + imgs[b].height;
+		  //imgs[b].src = 'https://via.placeholder.com/' + imgs[b].width + 'x' + imgs[b].height;
+		}catch(e){console.log(e);}
+	}
+}
+changeImages();
